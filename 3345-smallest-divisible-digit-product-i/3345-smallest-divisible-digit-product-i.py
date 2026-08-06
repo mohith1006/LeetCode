f69b713ab6804b00d027464d49d3fdbc101c6e2d
AWALKAR MOHITH KUMAR <mohithawalkar17@gmail.com>
2026-08-06 23:20:51 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.98%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            k=1
+            for i in str(n):
+                k*=int(i)
+            if k%t==0:
+                return n
+            n+=1
+
+        
+
